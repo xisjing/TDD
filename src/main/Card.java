fb379fdc5923dfbb4f9cd85d932be22553be52cb
xisijing <xisijing@gmail.com>
2015-06-14 14:43:43 +0200

    implement class Card

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,5 +1,33 @@
 package main;
 
+import main.Card;
+
 public class Card {
 
+	private int suit;
+	private int number;
+	private boolean turnOn;
+
+	public Card(int suit, int number, boolean turnOn) {
+		this.suit = suit;
+		this.number = number;
+		this.turnOn = turnOn;
+	}
+
+	public boolean turnOn() {
+		return turnOn;
+	}
+
+
+	public void overTurn() {
+		this.turnOn = !this.turnOn;
+	}
+	
+	@Override
+	public boolean equals(Object object){
+		assert object instanceof Card;
+		Card card = (Card) object;
+		return card.suit == suit && card.number == number;
+		
+	}
 }
