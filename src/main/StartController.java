086596d5108a2a3a8e853de725fe56ea6229159b
xisijing <xisijing@gmail.com>
2015-06-14 14:49:35 +0200

    getPeekTableausTest() is tested

--- src/main/StartController.java
+++ src/main/StartController.java
@@ -1,5 +1,24 @@
 package main;
 
+import java.util.ArrayList;
+
+import main.Card;
+
 public class StartController {
+
+	public ArrayList<Card> getPeekTableaus() {
+		
+		ArrayList<Card> peekTableaus = new ArrayList<Card>();
+		
+		peekTableaus.add(new Card(0,5,true)); 
+		peekTableaus.add(new Card(0,5,true)); 
+		peekTableaus.add(new Card(0,5,true)); 
+		peekTableaus.add(new Card(0,5,true)); 
+		peekTableaus.add(new Card(0,5,true)); 
+		peekTableaus.add(new Card(0,5,true));
+		peekTableaus.add(new Card(0,5,true)); 
+	
+		return peekTableaus;
+	}
 
 }
--- src/test/StartControllerTest.java
+++ src/test/StartControllerTest.java
@@ -2,13 +2,31 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import main.Card;
+import main.StartController;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class StartControllerTest {
 
+	private StartController startController;
+	
+	@Before
+	public void init(){
+		startController = new StartController();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getPeekTableausTest() {
+		ArrayList<Card> peekTableaus = startController.getPeekTableaus();
+		assertEquals(7, peekTableaus.size());
+		for(Card card : peekTableaus){
+			assertTrue(card.turnOn());
+		}
+			
 	}
 
 }
